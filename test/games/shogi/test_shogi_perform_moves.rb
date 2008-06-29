d193d4067416e49028cb6891c72b4c25fce6706d
Paolo Capriotti <p.capriotti@gmail.com>
2008-06-29 01:16:34 +0100

    Test shogi pawn capture.

--- test/games/shogi/test_shogi_perform_moves.rb
+++ test/games/shogi/test_shogi_perform_moves.rb
@@ -21,4 +21,17 @@ class TestShogiPerformMoves < Test::Unit::TestCase
     assert_no_piece 2, 6
     assert_equal :white, @state.turn
   end
+  
+  def test_pawn_capture
+    execute 2, 6, 2, 5
+    execute 2, 2, 2, 3
+    execute 2, 5, 2, 4
+    execute 2, 3, 2, 4
+    
+    assert_piece :white, :pawn, 2, 4
+    assert_no_piece 2, 3
+    assert_no_piece 2, 2
+    assert_no_piece 2, 5
+    assert_no_piece 2, 6
+  end
 end
