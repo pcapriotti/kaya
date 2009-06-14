df8358a50fdaa8075e43940189d09c88556e94f6
Paolo Capriotti <p.capriotti@gmail.com>
2009-06-14 11:57:53 +0200

    Remove debug output.

--- lib/ics/style12.rb
+++ lib/ics/style12.rb
@@ -64,13 +64,11 @@ class Style12
   def self.from_match(match, games)
     game_number = match[GAME_NUMBER].to_i
     current_game = games[game_number]
-    puts "current_game = #{current_game.inspect}"
     game = if current_game
              current_game[:game]
            else
              Game.dummy
            end
-    puts "game = #{game}"
     icsapi = ICSApi.new(game)
 
     state = 
