08e09ee538f0a10be528beb19f4f29b33683d2c8
Paolo Capriotti <p.capriotti@gmail.com>
2009-06-29 02:18:44 +0200

    Move shebang line before copyright header.

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # Copyright (c) 2009 Paolo Capriotti <p.capriotti@gmail.com>
 # 
 # This program is free software; you can redistribute it and/or modify
@@ -5,7 +6,6 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-#!/usr/bin/env ruby
 require '${DATA_INSTALL_DIR}/kaya/lib/kaya.rb'
 
 start_kaya if __FILE__ == $0
