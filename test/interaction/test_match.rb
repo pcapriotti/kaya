97e2df0f2005b8ed8a0274375870c8c570942739
Paolo Capriotti <p.capriotti@gmail.com>
2009-06-29 02:05:49 +0200

    Add licence and installation instructions.

--- test/interaction/test_match.rb
+++ test/interaction/test_match.rb
@@ -1,3 +1,10 @@
+# Copyright (c) 2009 Paolo Capriotti <p.capriotti@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 require 'test/unit'
 require 'interaction/match'
 require 'games/all'
