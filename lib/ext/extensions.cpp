bfd6838c9ab63225e53a9905256efb1148fda4cd
Paolo Capriotti <p.capriotti@gmail.com>
2009-07-12 01:52:55 +0200

    Add compile time switch to enable ruby-only backtraces.

--- lib/ext/extensions.cpp
+++ lib/ext/extensions.cpp
@@ -9,6 +9,10 @@
 
 #include <QCoreApplication>
 #include <kdemacros.h>
+#include <ruby.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
 
 Extensions::Extensions(QObject* parent)
 : QObject(parent) { }
@@ -19,6 +23,17 @@ static void init()
     ext->setObjectName("kaya extensions");
 }
 
+#ifdef RUBY_EXCEPTIONS_ONLY
+static void crash_handler(int)
+{
+    rb_raise(rb_eException, "Native code crash!");
+}
+#endif
+
 extern "C" KDE_EXPORT void Init_extensions() {
     init();
+
+#ifdef RUBY_EXCEPTIONS_ONLY
+    signal(SIGSEGV, crash_handler);
+#endif
 }
