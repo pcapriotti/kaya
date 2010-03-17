af65195b26dd4715577fd598e3727d25cd95d745
Paolo Capriotti <p.capriotti@gmail.com>
2010-03-17 18:58:21 +0000

    Add plugin example.

---
+++ examples/helloworld/hello_world.rb
@@ -0,0 +1,27 @@
+require 'plugins/plugin'
+
+class HelloWorldPlugin
+  include Plugin
+  include ActionProvider
+
+  plugin :name => 'Hello World',
+         :interface => :action_provider,
+         :bundle => 'helloworld'
+
+  def initialize
+    action(:hello_world,
+           :text => KDE.i18n('Say &Hello World')) do |parent|
+      parent.console.append("Hello world")
+    end
+  end
+
+  def gui
+    KDE::gui(:hello_world) do |g|
+      g.menubar do |mb|
+        mb.menu(:gameMenu) do |m|
+          m.action :hello_world
+        end
+      end
+    end
+  end
+end
